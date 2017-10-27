65f112ec34bc61ca2a8bc1575d11f5c7c54f5746
Matias Crespi <mcrespi@modernizacion.gob.ar>
2017-10-27 17:01:07 -0300

    chore: update dependencies

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-import Promise from 'bluebird';
-import mongoose from 'mongoose';
-import config from './config/env';
-import app from './config/express';
+import Promise                  from 'bluebird';
+import mongoose                 from 'mongoose';
+import config                   from './config/env';
+import app                      from './config/express';
 
 // promisify mongoose
 Promise.promisifyAll(mongoose);
 
 // connect to mongo db
-mongoose.connect(config.db, { server: { socketOptions: { keepAlive: 1 } } });
+mongoose.connect(config.db, { useMongoClient: true, keepAlive: true, })
 mongoose.connection.on('error', () => {
 	throw new Error(`unable to connect to database: ${config.db}`);
 });
 
-const debug = require('debug')('express-mongoose-es6-rest-api:index');
+const debug = require('debug')('node_scaffolding:index');
 
 // listen on port config.port
 app.listen(config.port, () => {
