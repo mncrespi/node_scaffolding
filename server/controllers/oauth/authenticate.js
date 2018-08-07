60d968c09e57c65884283ef594115aea3d132e73
Matias Crespi <mat.cres@gmail.com>
2018-08-07 11:29:11 -0300

    chore: autenticate

--- server/controllers/oauth/authenticate.js
+++ server/controllers/oauth/authenticate.js
@@ -1,36 +1,29 @@
-// import oauthServer from 'oauth2-server'
-// const Request = oauthServer.Request
-// const Response = oauthServer.Response
 import { Request, Response, } from 'oauth2-server'
 import oAuth from './oauth'
-import APIResponse from '../../helpers/APIStandarResponses'
 import logger from '../../../config/winston'
 
 
+/**
+ * Authentication Middleware
+ * @param options
+ * @returns {Function}
+ */
 export default function (options) {
   return function (req, res, next) {
-    const request = new Request({
-      // headers: {
-      //   authorization: req.headers.authorization,
-      // },
-      method: req.method,
-      headers: req.headers,
-      query: req.query,
-      body: req.body,
-    })
+    const request = new Request(req)
     const response = new Response(res)
 
     oAuth.authenticate(request, response, options = {})
       .then((token) => {
         // Request is authorized.
-        logger.log('debug', 'Request is authorized::Token::%j', token)
+        logger.log('silly', 'Request is authorized::Token::%j', token)
         req.user = token
-        next()
+        return next()
       })
       .catch((err) => {
         // Request is not authorized.
-        logger.log('debug', 'Request is not authorized::Error::%j', err)
-        res.status(err.code || 500).json(APIResponse.error(err.code, err.message, err.name, err.code))
+        logger.log('silly', 'Request is not authorized::Error::%j', err)
+        return next(err)
       })
   }
 }
