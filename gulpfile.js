3dd53e793a1e2190d0a7974cafe2015ceb32af72
Matias Crespi <mat.cres@gmail.com>
2019-09-25 12:25:01 +0200

    feat: gulp linter

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,29 @@
-// const eslint = require("gulp-eslint")
+const gulp = require('gulp')
+const eslint = require('gulp-eslint')
+const gulpIf = require('gulp-if')
+
+
+const paths = {
+  js: [
+    './**/*.js',
+    '!node_modules/**',
+    '!assets/**',
+  ],
+  nonJs: [
+    './package.json',
+    './package-lock.json',
+    './.gitignore',
+  ],
+  tests: './server/tests/*.js',
+}
+
+
+function isFixed(file) {
+  // Has ESLint fixed the file contents?
+  return file.eslint != null && file.eslint.fixed
+}
+
+
 
 function defaultTask(cb) {
   // place code for your default task here
@@ -7,17 +32,16 @@ function defaultTask(cb) {
 
 
 // Lint scripts
-// function linter() {
-//   return gulp
-//     .src([
-//       './assets/js/**/*',
-//       './gulpfile.js',
-//     ])
-//     .pipe(plumber())
-//     .pipe(eslint())
-//     .pipe(eslint.format())
-//     .pipe(eslint.failAfterError());
-// }
-
+function lint() {
+  return gulp
+    .src(paths.js)
+      .pipe(eslint({
+        fix: (process.env.LINT_FIX),
+      }))
+      .pipe(eslint.format())
+      .pipe(gulpIf(isFixed, gulp.dest('./')))
+      .pipe(eslint.failAfterError())
+}
 
 exports.default = defaultTask
+exports.linter = lint
